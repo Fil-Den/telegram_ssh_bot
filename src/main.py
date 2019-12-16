e554d285cee06804723a4dd3829bddb86bd487f6
Fil <owl.flyight@gmail.com>
2019-12-16 10:03:42 +0300

    add main for test

--- src/main.py
+++ src/main.py
@@ -7,6 +7,14 @@ set messages and commands handlers
 import json
 import logging
 import argparse
+from bot import (
+    TelegramBot, MsgHandler
+)
+from system import system
+from configJsonSchema import CONFIG_JSON_SCHEMA
+from jsonschema import (
+    validate, ValidationError
+)
 
 
 def getPathToCfg():
@@ -25,6 +33,11 @@ def getPathToCfg():
     return args.config
 
 
+def runAllCommand(command):
+    '''run command and return result'''
+    return system.runCommand(command)
+
+
 def main():
     '''programm entry point'''
     cfgPath = getPathToCfg()
@@ -34,8 +47,41 @@ def main():
         format='{ "time": "%(asctime)s",  "level": "%(levelname)s", ' +
                '"msg": "%(message)s" }', level=logging.INFO)
 
-    logging.info('cfgPath = ' + cfgPath)
+    # load and validate config
+    with open(cfgPath) as file:
+        cfg = json.load(file)
+    try:
+        validate(instance=cfg, schema=CONFIG_JSON_SCHEMA)
+    except ValidationError:
+        logging.error("config parse error! (See example in README).")
+        return
+
+    # run bot
+    bot = TelegramBot(
+        cfg['telegram']['api_token'],
+        cfg['telegram']['proxy_url'],
+        cfg['telegram']['proxy_username'],
+        cfg['telegram']['proxy_password'])
+    allowed_users = cfg['telegram']['allowed_users']
+    # add msg handler
+    if cfg['allow_all_commands']:
+        bot.set_message_handler(
+            MsgHandler(
+                (lambda cmd: system.runCommand(cmd)),
+                allowed_users=allowed_users
+            )
+        )
+    # add commands
+    for cmd in cfg['commands']:
+        bot.set_command_handler(
+            cmd['command_name'],
+            MsgHandler(
+                (lambda cmd: system.runCommand(cmd['command'])),
+                allowed_users=allowed_users
+            )
+        )
+    bot.start_polling()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
