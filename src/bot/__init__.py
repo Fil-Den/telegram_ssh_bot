ccc83ad27243c2310144dfe92b3e48394f36cc97
Fil <owl.flyight@gmail.com>
2020-03-23 23:11:37 +0300

    Fix comment firs character and end marks.

--- src/bot/__init__.py
+++ src/bot/__init__.py
@@ -1,3 +1,3 @@
-'''for more usable import from that's packat'''
+'''For more usable import from that's packet.'''
 from .bot import TelegramBot
 from .handlers import MsgHandler
--- src/bot/bot.py
+++ src/bot/bot.py
@@ -1,4 +1,4 @@
-'''realize api for bot working'''
+'''Realize api for bot working.'''
 import logging
 from telegram.ext import (
     Updater, CommandHandler,
@@ -7,10 +7,10 @@ from telegram.ext import (
 
 
 class TelegramBot:
-    '''class for work with telegram'''
+    '''Class for work with telegram.'''
     def __init__(self, token, proxy_url,
                  username=None, passord=None, connection_timeout=7):
-        '''initialize variables'''
+        '''Initialize variables.'''
         if proxy_url:
             request_kwargs = {
                 'proxy_url': proxy_url,
@@ -27,17 +27,17 @@ class TelegramBot:
             self._updater = Updater(token=token, use_context=True)
 
     def set_command_handler(self, command, handler):
-        '''set command handler function'''
+        '''Set command handler function.'''
         cmd_handler = CommandHandler(command, handler)
         self._updater.dispatcher.add_handler(cmd_handler)
 
     def set_message_handler(self, msg_handler):
-        '''set start_handler function on messages'''
+        '''Set start_handler function on messages.'''
         echo_handler = MessageHandler(Filters.text, msg_handler)
         self._updater.dispatcher.add_handler(echo_handler)
 
     def start_polling(self):
-        '''start pooling on updater and lock process'''
+        '''Start pooling on updater and lock process.'''
         self._updater.start_polling(bootstrap_retries=5)
         logging.info("Bot started!")
         self._updater.idle()
